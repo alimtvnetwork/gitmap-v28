46d654fd06153e550ed287b0c4ed0ad18b008082
MD ALIM UL KARIM <devorg.bd@gmail.com>
2026-08-31 16:01:37 +0800

    release: v6.155.6 - provide rich help, descriptions, and examples when search commands receive no arguments

--- gitmap/constants/constants.go
+++ gitmap/constants/constants.go
@@ -14,7 +14,7 @@ package constants
 
 // Version holds the current release version of gitmap.
 // This is overridden at compile time via -ldflags during CI/CD.
-var Version = "v6.155.5" // Overridden at compile time via -ldflags
+var Version = "v6.155.6" // Overridden at compile time via -ldflags
 
 // RepoPath is set at build time via -ldflags.
 var RepoPath = ""
