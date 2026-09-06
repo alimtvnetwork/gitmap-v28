8f99c41d316fe7dc89ecf88a2292d9bc56a6f763
MD ALIM UL KARIM <devorg.bd@gmail.com>
2026-09-06 12:32:44 +0800

    refactor(db-reset): align naming convention in cmddb_reset

--- gitmap/cmd/cmddb_reset.go
+++ gitmap/cmd/cmddb_reset.go
@@ -9,35 +9,38 @@ import (
 	"github.com/alimtvnetwork/gitmap-v28/gitmap/store"
 )
 
-func runDBResetAction(args []string) error {
+func runDbResetAction(args []string) error {
 	msg := constants.ColorYellow + "Are you sure you want to reset the database? All tracked repository records and split databases will be cleared. [y/N]: " + constants.ColorReset
 	if !confirmOrSkip(msg, args) {
 		fmt.Println(constants.ColorDim + "Database reset canceled." + constants.ColorReset)
+
 		return nil
 	}
-	return performDBReset()
+
+	return performDbReset()
 }
 
-func performDBReset() error {
-	mainDB, err := store.OpenDefault()
+func performDbReset() error {
+	mainDb, err := store.OpenDefault()
 	if err != nil {
 		return apperror.WrapSimple(err, "E9001")
 	}
-	defer mainDB.Close()
+	defer mainDb.Close()
 
-	if resetErr := mainDB.Reset(); resetErr != nil {
+	if resetErr := mainDb.Reset(); resetErr != nil {
 		return apperror.WrapSimple(resetErr, "E9002")
 	}
-	removedSplit := clearSplitDBFiles()
+	removedSplit := clearSplitDbFiles()
 
 	fmt.Printf("%s✓ Main database reset: %s%s\n", constants.ColorGreen, store.DefaultDBPath(), constants.ColorReset)
 	fmt.Printf("%s✓ Cleared %d split repository database(s)%s\n", constants.ColorGreen, removedSplit, constants.ColorReset)
 	fmt.Printf("%s✓ Database reset successfully completed.%s\n", constants.ColorGreen, constants.ColorReset)
+
 	return nil
 }
 
-func clearSplitDBFiles() int {
-	dirs := findSplitDBDirs()
+func clearSplitDbFiles() int {
+	dirs := findSplitDbDirs()
 	count := 0
 	for _, dir := range dirs {
 		entries, err := os.ReadDir(dir)
@@ -48,6 +51,7 @@ func clearSplitDBFiles() int {
 			count += removeSingleSplitEntry(dir, e)
 		}
 	}
+
 	return count
 }
 
@@ -59,5 +63,13 @@ func removeSingleSplitEntry(dir string, e os.DirEntry) int {
 	if rmErr := os.Remove(target); rmErr == nil {
 		return 1
 	}
+
 	return 0
 }
+
+// Backwards-compatible aliases
+var (
+	runDBResetAction  = runDbResetAction
+	performDBReset    = performDbReset
+	clearSplitDBFiles = clearSplitDbFiles
+)
